9d7be97447c71a9e36cbeb951a1cff7a938ca406
Matt Hart <mp.hart047@gamil.com>
2021-03-08 19:41:03 -0600

    Methods Exersize 3 v1.0

---
+++ MethodExersize3/MethodExersize3/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace MethodExersize3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EqualOrNot();
+            EvenOrOdd();
+            PossitiveOrNegative();
+            CanYouVote();
+            
+            for (int i = -1000; i <= 1000; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 3; i >= -999; i -= 3) //Goes from 3 down to negative 999.
+            {
+                Console.WriteLine(i);
+            }
+
+            static void EqualOrNot()
+            {
+                Console.WriteLine("Give me two number.");
+                int x = int.Parse(Console.ReadLine());
+                int y = int.Parse(Console.ReadLine());
+                if (x == y)
+                {
+                    Console.WriteLine("The are equal.");
+                }
+                else
+                {
+                    Console.WriteLine("They are not equal");
+                }
+            }
+
+
+            static void EvenOrOdd()
+            {
+                Console.WriteLine("Give me a number.");
+                int x = int.Parse(Console.ReadLine());
+
+                if (x % 2 == 0)
+                {
+                    Console.WriteLine("The number is even.");
+                }
+                else
+                {
+                    Console.WriteLine("The number is odd.");
+                }
+            }
+
+
+            static void PossitiveOrNegative()
+            {
+                Console.WriteLine("Give me a number.");
+                int x = int.Parse(Console.ReadLine());
+                if (x > 0)
+                {
+                    Console.WriteLine("The number is possitive!!");
+                }
+                else
+                {
+                    Console.WriteLine("The number is negative.");
+                }
+            }
+
+            
+            static void CanYouVote()
+            {
+                
+                Console.WriteLine("How old are you?");
+                var age = int.Parse(Console.ReadLine());
+
+                if (age >= 18)
+                {
+                    Console.WriteLine("You are ready to vote my friend!!!");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry bud, you got a few more years..");
+                }
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
